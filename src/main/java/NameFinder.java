c771c93d95ba65a77a015a71f379b3a14b6c6696
Marco Arnaboldi <mr.arnaboldi@gmail.com>
2016-09-29 11:59:45 -0500

    Added more name finder model in order to retrieve more names

--- src/main/java/NameFinder.java
+++ src/main/java/NameFinder.java
@@ -1,5 +1,7 @@
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
@@ -28,42 +30,56 @@ public class NameFinder
 
         InputStream modelInToken = null;
         InputStream modelIn = null;
+        InputStream modelInSentence = null;
 
         try {
 
             String tokenModelPAth = this.getClass().getResource("en-token.bin").toURI().getPath();
-            String nameModelPAth = this.getClass().getResource("en-ner-person.bin").toURI().getPath();
+            String engNameModelPAth = this.getClass().getResource("en-ner-person.bin").toURI().getPath();
+            String esNameModelPAth = this.getClass().getResource("es-ner-person.bin").toURI().getPath();
+            String nlNameModelPAth = this.getClass().getResource("nl-ner-person.bin").toURI().getPath();
 
 
-            //1. convert sentence into tokens
-            modelInToken = new FileInputStream(tokenModelPAth);
-            TokenizerModel modelToken = new TokenizerModel(modelInToken);
-            Tokenizer tokenizer = new TokenizerME(modelToken);
-            String tokens[] = tokenizer.tokenize(text);
+            String sentenceModelPAth = this.getClass().getResource("en-sent.bin").toURI().getPath();
+
+
+            //0. convert text into sentence
+            modelInSentence = new FileInputStream(sentenceModelPAth);
+            SentenceModel modelSentence = new SentenceModel(modelInSentence);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(modelSentence);
+            String sentences[] = sentenceDetector.sentDetect(text);
 
 
 
+            for (String sentence : sentences)
+            {
+                //System.out.println(sentence);
+                //1. convert sentence into tokens
+                modelInToken = new FileInputStream(tokenModelPAth);
+                TokenizerModel modelToken = new TokenizerModel(modelInToken);
+                Tokenizer tokenizer = new TokenizerME(modelToken);
+                String tokens[] = tokenizer.tokenize(sentence);
 
 
-            //2. find names
-            modelIn = new FileInputStream(nameModelPAth);
-            TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
-            NameFinderME nameFinder = new NameFinderME(model);
+                //2. find names
+                modelIn = new FileInputStream(engNameModelPAth);
+                TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+                NameFinderME nameFinder = new NameFinderME(model);
 
-            Span nameSpans[] = nameFinder.find(tokens);
+                Span nameSpans[] = nameFinder.find(tokens);
 
-            //find probabilities for names
-            double[] spanProbs = nameFinder.probs(nameSpans);
+                //find probabilities for names
+                double[] spanProbs = nameFinder.probs(nameSpans);
 
-            //3. print names
-            for( int i = 0; i<nameSpans.length; i++) {
-                System.out.println("Span: "+nameSpans[i].toString());
-                System.out.println("Covered text is: "+tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart()+1]);
-                System.out.println("Probability is: "+spanProbs[i]);
+                //3. print names
+                for( int i = 0; i<nameSpans.length; i++)
+                {
+                    //System.out.println("Span: "+nameSpans[i].toString());
+                    System.out.println("Covered text is: "+tokens[nameSpans[i].getStart()] + " " + tokens[nameSpans[i].getStart()+1]);
+                    System.out.println("Probability is: "+spanProbs[i]);
+                }
             }
-            //Span: [0..2) person
-            //Covered text is: Jack London
-            //Probability is: 0.7081556539712883
+
         }
         catch (Exception ex) {}
         finally {
